54594ba50107f1a570df94439f5d6928edd92d97
jozefhruska <me@jozefhruska.com>
2023-03-28 20:49:50 +0200

    fix: preview dark mode color

--- source/src/components/common/Preview.tsx
+++ source/src/components/common/Preview.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 export const Preview: React.FC<Props> = ({ header, value }) => (
-  <div className="rounded-md border border-slate-200 bg-slate-100 py-2 px-3 leading-normal">
+  <div className="rounded-md border border-slate-200 bg-slate-100 py-2 px-3 leading-normal dark:border-slate-700 dark:bg-slate-800">
     <strong className="font-medium">{header}:</strong> {value}
   </div>
 );
