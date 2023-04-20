8c1e7a08af51ef029b7344b512b1c1d7136219ec
jozefhruska <me@jozefhruska.com>
2023-04-20 15:38:07 +0200

    feat(thesis): ui screenshots

--- source/src/manifest.ts
+++ source/src/manifest.ts
@@ -7,9 +7,9 @@ const manifest: Omit<
   action: {
     default_icon: {
       16: 'icons/16.png',
-      19: 'icons/19.png',
       32: 'icons/32.png',
-      38: 'icons/38.png',
+      48: 'icons/48.png',
+      128: 'icons/128.png',
     },
     default_popup: 'src/entries/popup/index.html',
   },
@@ -30,14 +30,9 @@ const manifest: Omit<
   host_permissions: ['*://*/*'],
   icons: {
     16: 'icons/16.png',
-    19: 'icons/19.png',
     32: 'icons/32.png',
-    38: 'icons/38.png',
     48: 'icons/48.png',
-    64: 'icons/64.png',
-    96: 'icons/96.png',
     128: 'icons/128.png',
-    256: 'icons/256.png',
     512: 'icons/512.png',
   },
   // options_ui: {
