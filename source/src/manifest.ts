537ba2c8ac2759aa074d4cea95615e835ad5d965
jozefhruska <me@jozefhruska.com>
2023-04-29 13:08:38 +0200

    chore: basic firefox support

--- source/src/manifest.ts
+++ source/src/manifest.ts
@@ -1,5 +1,9 @@
 import pkg from '../package.json';
 
+const BUILD_TARGET = (process.env.BUILD_TARGET || 'chrome') as
+  | 'chrome'
+  | 'firefox';
+
 const manifest: Omit<
   chrome.runtime.ManifestV3,
   'name' | 'version' | 'manifest_version'
@@ -13,15 +17,22 @@ const manifest: Omit<
     },
     default_popup: 'src/entries/popup/index.html',
   },
-  background: {
-    service_worker: 'src/entries/background/main.ts',
-  },
+  ...(BUILD_TARGET === 'chrome'
+    ? {
+        background: {
+          service_worker: 'src/entries/background/main.ts',
+        },
+      }
+    : {}),
   // content_scripts: [
   //   {
   //     js: ['src/entries/contentScript/primary/main.tsx'],
   //     matches: ['*://*/*'],
   //   },
   // ],
+  content_security_policy: {
+    extension_pages: "default-src 'self'",
+  },
   permissions: [
     'declarativeNetRequest',
     'declarativeNetRequestFeedback',
@@ -34,6 +45,11 @@ const manifest: Omit<
     48: 'icons/48.png',
     128: 'icons/128.png',
     512: 'icons/512.png',
+  },
+  browser_specific_settings: {
+    gecko: {
+      id: 'pfpp-extension@example.org',
+    },
   },
   // options_ui: {
   //   page: 'src/entries/options/index.html',
---
+++ source/webext.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Global options:
+  verbose: true,
+  // Command options:
+  build: {
+    overwriteDest: true,
+  },
+  run: {
+    firefox: 'nightly',
+  },
+};
